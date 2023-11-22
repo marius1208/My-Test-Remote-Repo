c6e8da023f660d1711335406a7c4f7ebe4f5e8a4
Marius <mariusteor@uni.coventry.ac.uk>
2023-11-22 12:10:16 +0000

    removed test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-word = "Banana"
-unique_letters = set(word)            
-num_letters = len(unique_letters)                            #The number of UNIQUE letters in the word that have not been guessed yet
-print(num_letters)
